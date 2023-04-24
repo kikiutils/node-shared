fae0caab909ec2db75f34d5918945c9da28de579
kiki-kanri <a470666@gmail.com>
2023-04-24 16:18:05 +0800

    Add isdir and isfile functions.

--- src/file.ts
+++ src/file.ts
@@ -1,10 +1,44 @@
 import {
 	readFile as _readFile,
 	readFileSync as _readFileSync,
+	stat as _stat,
+	statSync as _statSync,
 	writeFile as _writeFile,
 	writeFileSync as _writeFileSync,
 	WriteFileOptions
 } from 'fs-extra';
+
+export const isDir = async (path: string) => {
+	try {
+		const stats = await _stat(path);
+		return stats.isDirectory();
+	} catch(error) { }
+	return false;
+}
+
+export const isDirSync = (path: string) => {
+	try {
+		const stats = _statSync(path);
+		return stats.isDirectory();
+	} catch(error) { }
+	return false;
+}
+
+export const isFile = async (path: string) => {
+	try {
+		const stats = await _stat(path);
+		return stats.isFile();
+	} catch(error) { }
+	return false;
+}
+
+export const isFileSync = (path: string) => {
+	try {
+		const stats = _statSync(path);
+		return stats.isFile();
+	} catch(error) { }
+	return false;
+}
 
 export const readFile = async (path: string) => {
 	try {
