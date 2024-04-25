0ae226296fb58682f1ff81647a739246526bd629
kiki-kanri <a470666@gmail.com>
2024-04-26 00:42:55 +0800

    fix: correct return type error in enum utils

--- src/enum.ts
+++ src/enum.ts
@@ -1,2 +1,2 @@
-export const getEnumNumberValues = (data: Record<number | string, number | string>) => Object.values(data).filter((value) => typeof value === 'number');
-export const getEnumStringValues = (data: Record<number | string, number | string>) => Object.values(data).filter((value) => typeof value === 'string');
+export const getEnumNumberValues = (data: Record<number | string, number | string>) => Object.values(data).filter((value) => typeof value === 'number') as number[];
+export const getEnumStringValues = (data: Record<number | string, number | string>) => Object.values(data).filter((value) => typeof value === 'string') as string[];
