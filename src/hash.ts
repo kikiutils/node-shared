350907c6571bcb8a67fc201da074f75b72a17d9c
kiki-kanri <a470666@gmail.com>
2024-05-12 17:05:18 +0800

    feat: add Uint8Array as an input type for SHA3 related functions

--- src/hash.ts
+++ src/hash.ts
@@ -1,7 +1,7 @@
 import { sha3_224, sha3_256, sha3_384, sha3_512 } from '@noble/hashes/sha3';
 import { bytesToHex } from '@noble/hashes/utils';
 
-export const sha3224 = (text: string) => bytesToHex(sha3_224(text));
-export const sha3256 = (text: string) => bytesToHex(sha3_256(text));
-export const sha3384 = (text: string) => bytesToHex(sha3_384(text));
-export const sha3512 = (text: string) => bytesToHex(sha3_512(text));
+export const sha3224 = (data: string | Uint8Array) => bytesToHex(sha3_224(data));
+export const sha3256 = (data: string | Uint8Array) => bytesToHex(sha3_256(data));
+export const sha3384 = (data: string | Uint8Array) => bytesToHex(sha3_384(data));
+export const sha3512 = (data: string | Uint8Array) => bytesToHex(sha3_512(data));
