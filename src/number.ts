17a40538483370a49fc4fff9527c7e162c2709a0
kiki-kanri <a470666@gmail.com>
2024-07-11 12:03:20 +0800

    refactor(number)!: rename `toPercentString` to `calculateToPercentageString` and rewrite calculations using decimal.js

--- src/number.ts
+++ src/number.ts
@@ -1,8 +1,19 @@
+import Decimal from 'decimal.js';
 import { millify } from 'millify';
 
-export const prettyNumberToString = (value: number, options?: Parameters<typeof millify>[1]) => millify(value, { lowercase: true, precision: 2, ...options });
-export const toPercentString = (molecular: number, denominator: number, withSymbol: boolean = true) => {
-	const calculationResult = molecular / denominator;
-	const result = isNaN(calculationResult) ? '0' : (calculationResult * 100).toFixed(2).replace(/\.?0+$/, '');
-	return withSymbol ? `${result}%` : result;
+type CalculableValue = Decimal.Value | { toString(): string };
+
+export interface CalculateToPercentageStringOptions {
+	decimalPlaces?: number;
+	withSymbol?: boolean;
+}
+
+export const calculateToPercentageString = (molecular: CalculableValue, denominator: CalculableValue, options?: CalculateToPercentageStringOptions) => {
+	const molecularDecimal = new Decimal(molecular.toString());
+	const denominatorDecimal = new Decimal(denominator.toString());
+	const calculationResult = molecularDecimal.div(denominatorDecimal);
+	const result = calculationResult.isNaN() ? '0.00' : calculationResult.times(100).toFixed(options?.decimalPlaces || 2);
+	return options?.withSymbol ?? true ? `${result}%` : result;
 };
+
+export const prettyNumberToString = (value: number, options?: Parameters<typeof millify>[1]) => millify(value, { lowercase: true, precision: 2, ...options });
