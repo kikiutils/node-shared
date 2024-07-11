fd7374ddb4e57fb69c0af6ec581f4b87b26f7642
kiki-kanri <a470666@gmail.com>
2024-07-11 10:08:11 +0800

    feat(number): add options parameter to `prettyNumberToString` function

--- src/number.ts
+++ src/number.ts
@@ -1,6 +1,6 @@
 import { millify } from 'millify';
 
-export const prettyNumberToString = (value: number) => millify(value, { lowercase: true, precision: 2 });
+export const prettyNumberToString = (value: number, options?: Parameters<typeof millify>[1]) => millify(value, { lowercase: true, precision: 2, ...options });
 export const toPercentString = (molecular: number, denominator: number, withSymbol: boolean = true) => {
 	const calculationResult = molecular / denominator;
 	const result = isNaN(calculationResult) ? '0' : (calculationResult * 100).toFixed(2).replace(/\.?0+$/, '');
