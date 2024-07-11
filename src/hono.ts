43a355b7b6655b6e03f5e5a701210c47b7b4f36f
kiki-kanri <a470666@gmail.com>
2024-07-11 12:43:46 +0800

    feat(hono): add generic type parameter to `useHonoLogger` function

--- src/hono.ts
+++ src/hono.ts
@@ -1,9 +1,10 @@
 import type { HonoBase } from 'hono/hono-base';
 import { logger as honoLogger } from 'hono/logger';
+import type { Env, Schema } from 'hono/types';
 
 import logger from './logger';
 
-export const useHonoLogger = (honoApp: HonoBase, logIncoming: boolean = false) => {
+export const useHonoLogger = <E extends Env = Env, S extends Schema = {}, BasePath extends string = '/'>(honoApp: HonoBase<E, S, BasePath>, logIncoming: boolean = false) => {
 	if (process.env.NODE_ENV === 'production') return;
 	const logFunction = logIncoming ? (text: string) => logger.info(text.slice(6)) : (text: string) => !text.startsWith('  <--') && logger.info(text.slice(6));
 	honoApp.use(honoLogger(logFunction));
