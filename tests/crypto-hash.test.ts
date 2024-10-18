ec85e0cc458c1bf5d2e761f170e60f4763978677
kiki-kanri <a470666@gmail.com>
2024-10-18 11:47:45 +0800

    test: modify unit tests

--- tests/crypto-hash.test.ts
+++ tests/crypto-hash.test.ts
@@ -3,27 +3,27 @@ import * as cryptoHash from '../src/crypto-hash';
 describe('Crypto Hash Functions', () => {
 	const testData = 'test';
 	it('should generate correct MD5 hash', () => {
-		expect(cryptoHash.cryptoMd5(testData)).toBe('098f6bcd4621d373cade4e832627b4f6');
-		expect(cryptoHash.cryptoMd5ToBuffer(testData)).toEqual(Buffer.from('098f6bcd4621d373cade4e832627b4f6', 'hex'));
+		expect(cryptoHash.cryptoMD5(testData)).toBe('098f6bcd4621d373cade4e832627b4f6');
+		expect(cryptoHash.cryptoMD5ToBuffer(testData)).toEqual(Buffer.from('098f6bcd4621d373cade4e832627b4f6', 'hex'));
 	});
 
 	it('should generate correct SHA-3-224 hash', () => {
-		expect(cryptoHash.cryptoSha3224(testData)).toBe('3797bf0afbbfca4a7bbba7602a2b552746876517a7f9b7ce2db0ae7b');
-		expect(cryptoHash.cryptoSha3224ToBuffer(testData)).toEqual(Buffer.from('3797bf0afbbfca4a7bbba7602a2b552746876517a7f9b7ce2db0ae7b', 'hex'));
+		expect(cryptoHash.cryptoSHA3224(testData)).toBe('3797bf0afbbfca4a7bbba7602a2b552746876517a7f9b7ce2db0ae7b');
+		expect(cryptoHash.cryptoSHA3224ToBuffer(testData)).toEqual(Buffer.from('3797bf0afbbfca4a7bbba7602a2b552746876517a7f9b7ce2db0ae7b', 'hex'));
 	});
 
 	it('should generate correct SHA-3-256 hash', () => {
-		expect(cryptoHash.cryptoSha3256(testData)).toBe('36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80');
-		expect(cryptoHash.cryptoSha3256ToBuffer(testData)).toEqual(Buffer.from('36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80', 'hex'));
+		expect(cryptoHash.cryptoSHA3256(testData)).toBe('36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80');
+		expect(cryptoHash.cryptoSHA3256ToBuffer(testData)).toEqual(Buffer.from('36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80', 'hex'));
 	});
 
 	it('should generate correct SHA-3-384 hash', () => {
-		expect(cryptoHash.cryptoSha3384(testData)).toBe('e516dabb23b6e30026863543282780a3ae0dccf05551cf0295178d7ff0f1b41eecb9db3ff219007c4e097260d58621bd');
-		expect(cryptoHash.cryptoSha3384ToBuffer(testData)).toEqual(Buffer.from('e516dabb23b6e30026863543282780a3ae0dccf05551cf0295178d7ff0f1b41eecb9db3ff219007c4e097260d58621bd', 'hex'));
+		expect(cryptoHash.cryptoSHA3384(testData)).toBe('e516dabb23b6e30026863543282780a3ae0dccf05551cf0295178d7ff0f1b41eecb9db3ff219007c4e097260d58621bd');
+		expect(cryptoHash.cryptoSHA3384ToBuffer(testData)).toEqual(Buffer.from('e516dabb23b6e30026863543282780a3ae0dccf05551cf0295178d7ff0f1b41eecb9db3ff219007c4e097260d58621bd', 'hex'));
 	});
 
 	it('should generate correct SHA-3-512 hash', () => {
-		expect(cryptoHash.cryptoSha3512(testData)).toBe('9ece086e9bac491fac5c1d1046ca11d737b92a2b2ebd93f005d7b710110c0a678288166e7fbe796883a4f2e9b3ca9f484f521d0ce464345cc1aec96779149c14');
-		expect(cryptoHash.cryptoSha3512ToBuffer(testData)).toEqual(Buffer.from('9ece086e9bac491fac5c1d1046ca11d737b92a2b2ebd93f005d7b710110c0a678288166e7fbe796883a4f2e9b3ca9f484f521d0ce464345cc1aec96779149c14', 'hex'));
+		expect(cryptoHash.cryptoSHA3512(testData)).toBe('9ece086e9bac491fac5c1d1046ca11d737b92a2b2ebd93f005d7b710110c0a678288166e7fbe796883a4f2e9b3ca9f484f521d0ce464345cc1aec96779149c14');
+		expect(cryptoHash.cryptoSHA3512ToBuffer(testData)).toEqual(Buffer.from('9ece086e9bac491fac5c1d1046ca11d737b92a2b2ebd93f005d7b710110c0a678288166e7fbe796883a4f2e9b3ca9f484f521d0ce464345cc1aec96779149c14', 'hex'));
 	});
 });
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { flattenToSingleValue } from '../src';
+import { flattenToSingleValue, setReadonlyConstantToGlobalThis } from '../src';
 
 describe('flattenToSingleValue', () => {
 	test('should return the first element of an array', () => {
@@ -38,5 +38,36 @@ describe('flattenToSingleValue', () => {
 		const value: undefined = undefined;
 		const defaultValue = 'default';
 		expect(flattenToSingleValue(value, defaultValue)).toBe(defaultValue);
+	});
+});
+
+describe('setReadonlyConstantToGlobalThis', () => {
+	const constantValue = 'testValue';
+	it('should define a readonly constant on globalThis', () => {
+		const constantName = Math.random().toString(36).substring(2, 15);
+		setReadonlyConstantToGlobalThis(constantName, constantValue);
+		expect((globalThis as any)[constantName]).toBe(constantValue);
+		expect(() => ((globalThis as any)[constantName] = 'newValue')).toThrow(TypeError);
+		expect((globalThis as any)[constantName]).toBe(constantValue);
+	});
+
+	it('should not be configurable', () => {
+		const constantName = Math.random().toString(36).substring(2, 15);
+		setReadonlyConstantToGlobalThis(constantName, constantValue);
+		const deleteResult = delete (globalThis as any)[constantName];
+		expect(deleteResult).toBe(false);
+		expect((globalThis as any)[constantName]).toBe(constantValue);
+	});
+
+	it('should allow passing additional attributes', () => {
+		const constantName = Math.random().toString(36).substring(2, 15);
+		setReadonlyConstantToGlobalThis(constantName, constantValue, { enumerable: true });
+		expect(Object.getOwnPropertyDescriptor(globalThis, constantName)?.enumerable).toBe(true);
+	});
+
+	it('should not be writable', () => {
+		const constantName = Math.random().toString(36).substring(2, 15);
+		setReadonlyConstantToGlobalThis(constantName, constantValue);
+		expect(Object.getOwnPropertyDescriptor(globalThis, constantName)?.writable).toBe(false);
 	});
 });
