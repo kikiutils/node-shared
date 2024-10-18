da299034f008c662dd3bb34f6533af54b48fdb99
kiki-kanri <a470666@gmail.com>
2024-10-18 11:27:26 +0800

    refactor!: merge functions from object.ts into index.ts

--- src/object.ts
+++
@@ -1,8 +0,0 @@
-export const setReadonlyConstantToGlobalThis = (name: string, value: any, attributes?: PropertyDescriptor & ThisType<any>) => {
-	Object.defineProperty(globalThis, name, {
-		...attributes,
-		configurable: false,
-		value,
-		writable: false
-	});
-};
