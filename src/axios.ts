09af6a09c7e6306c6265e96113af7ce535407278
kiki-kanri <a470666@gmail.com>
2024-11-25 11:34:24 +0800

    refactor: simplify wrapper functions by removing async/await
    Refactored wrapper functions across the codebase to remove redundant async/await usage.
    
    These functions now directly return the underlying Promises.

--- src/axios.ts
+++ src/axios.ts
@@ -17,17 +17,17 @@ export type AxiosRequestData = AxiosRequestParams | FormData;
 export type AxiosRequestParams = Record<number | string, any>;
 
 export const axiosInstance: AxiosInstance = axios.create({});
-export const axiosDelete = async <T = string>(url: string, params?: AxiosRequestParams, config?: AxiosRequestConfig) => await axiosRequest<T>(url, 'delete', params, {}, config);
+export const axiosDelete = <T = string>(url: string, params?: AxiosRequestParams, config?: AxiosRequestConfig) => axiosRequest<T>(url, 'delete', params, {}, config);
 export const $axiosDelete = async <T = string>(url: string, params?: AxiosRequestParams, config?: AxiosRequestConfig) => (await axiosDelete<T>(url, params, config)).data;
-export const axiosGet = async <T = any>(url: string, params?: AxiosRequestParams, config?: AxiosRequestConfig) => await axiosRequest<T>(url, 'get', params, {}, config);
+export const axiosGet = <T = any>(url: string, params?: AxiosRequestParams, config?: AxiosRequestConfig) => axiosRequest<T>(url, 'get', params, {}, config);
 export const $axiosGet = async <T = any>(url: string, params?: AxiosRequestParams, config?: AxiosRequestConfig) => (await axiosGet<T>(url, params, config)).data;
-export const axiosPatch = async <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => await axiosRequest<T>(url, 'patch', {}, data, config);
+export const axiosPatch = <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => axiosRequest<T>(url, 'patch', {}, data, config);
 export const $axiosPatch = async <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => (await axiosPatch<T>(url, data, config)).data;
-export const axiosPost = async <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => await axiosRequest<T>(url, 'post', {}, data, config);
+export const axiosPost = <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => axiosRequest<T>(url, 'post', {}, data, config);
 export const $axiosPost = async <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => (await axiosPost<T>(url, data, config)).data;
-export const axiosPut = async <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => await axiosRequest<T>(url, 'put', {}, data, config);
+export const axiosPut = <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => axiosRequest<T>(url, 'put', {}, data, config);
 export const $axiosPut = async <T = string>(url: string, data?: AxiosRequestData, config?: AxiosRequestConfig<AxiosRequestData>) => (await axiosPut<T>(url, data, config)).data;
 
-export async function axiosRequest<T = any, R extends AxiosResponse<T> = AxiosResponse<T>, D extends AxiosRequestData = any>(url: string, method: Method, params?: AxiosRequestData, data?: D, config?: AxiosRequestConfig) {
-	return await axiosInstance.request<T, R, D>({ ...config, data, method, params, url });
+export function axiosRequest<T = any, R extends AxiosResponse<T> = AxiosResponse<T>, D extends AxiosRequestData = any>(url: string, method: Method, params?: AxiosRequestData, data?: D, config?: AxiosRequestConfig) {
+	return axiosInstance.request<T, R, D>({ ...config, data, method, params, url });
 }
