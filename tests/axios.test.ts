844051b4e1e04b16732953dedc37628476fef248
kiki-kanri <a470666@gmail.com>
2024-12-10 14:41:12 +0800

    refactor: change Url to URL

--- tests/axios.test.ts
+++ tests/axios.test.ts
@@ -4,104 +4,104 @@ const mockConfig = { headers: { 'Content-Type': 'application/json' } };
 const mockData = { key: 'value' };
 const mockParams = { param1: 'value1' };
 const mockResponse = { data: 'mocked data' };
-const mockUrl = '/mock-url';
+const mockURL = '/mock-url';
 
 describe('http utility functions', () => {
     beforeEach(() => axiosInstance.request = jest.fn().mockResolvedValue(mockResponse));
     // DELETE
     it('should make a DELETE request using axiosDelete', async () => {
-        await axiosDelete(mockUrl, mockParams, mockConfig);
+        await axiosDelete(mockURL, mockParams, mockConfig);
         expect(axiosInstance.request).toHaveBeenCalledWith({
             ...mockConfig,
             data: {},
             method: 'delete',
             params: mockParams,
-            url: mockUrl,
+            url: mockURL,
         });
     });
 
     it('should return data directly from DELETE request using $axiosDelete', async () => {
-        const data = await $axiosDelete(mockUrl, mockParams, mockConfig);
+        const data = await $axiosDelete(mockURL, mockParams, mockConfig);
         expect(data).toBe(mockResponse.data);
     });
 
     // GET
     it('should make a GET request using axiosGet', async () => {
-        await axiosGet(mockUrl, mockParams, mockConfig);
+        await axiosGet(mockURL, mockParams, mockConfig);
         expect(axiosInstance.request).toHaveBeenCalledWith({
             ...mockConfig,
             data: {},
             method: 'get',
             params: mockParams,
-            url: mockUrl,
+            url: mockURL,
         });
     });
 
     it('should return data directly from GET request using $axiosGet', async () => {
-        const result = await $axiosGet(mockUrl, mockParams, mockConfig);
+        const result = await $axiosGet(mockURL, mockParams, mockConfig);
         expect(result).toBe(mockResponse.data);
     });
 
     // PATCH
     it('should make a PATCH request using axiosPatch', async () => {
-        await axiosPatch(mockUrl, mockData, mockConfig);
+        await axiosPatch(mockURL, mockData, mockConfig);
         expect(axiosInstance.request).toHaveBeenCalledWith({
             ...mockConfig,
             data: mockData,
             method: 'patch',
             params: {},
-            url: mockUrl,
+            url: mockURL,
         });
     });
 
     it('should return data directly from PATCH request using $axiosPatch', async () => {
-        const result = await $axiosPatch(mockUrl, mockData, mockConfig);
+        const result = await $axiosPatch(mockURL, mockData, mockConfig);
         expect(result).toBe(mockResponse.data);
     });
 
     // POST
     it('should make a POST request using axiosPost', async () => {
-        await axiosPost(mockUrl, mockData, mockConfig);
+        await axiosPost(mockURL, mockData, mockConfig);
         expect(axiosInstance.request).toHaveBeenCalledWith({
             ...mockConfig,
             data: mockData,
             method: 'post',
             params: {},
-            url: mockUrl,
+            url: mockURL,
         });
     });
 
     it('should return data directly from POST request using $axiosPost', async () => {
-        const result = await $axiosPost(mockUrl, mockData, mockConfig);
+        const result = await $axiosPost(mockURL, mockData, mockConfig);
         expect(result).toBe(mockResponse.data);
     });
 
     // PUT
     it('should make a PUT request using axiosPut', async () => {
-        await axiosPut(mockUrl, mockData, mockConfig);
+        await axiosPut(mockURL, mockData, mockConfig);
         expect(axiosInstance.request).toHaveBeenCalledWith({
             ...mockConfig,
             data: mockData,
             method: 'put',
             params: {},
-            url: mockUrl,
+            url: mockURL,
         });
     });
 
     it('should return data directly from PUT request using $axiosPut', async () => {
-        const result = await $axiosPut(mockUrl, mockData, mockConfig);
+        const result = await $axiosPut(mockURL, mockData, mockConfig);
         expect(result).toBe(mockResponse.data);
     });
 
     // request
     it('should make a PUT request using axiosRequest', async () => {
-        await axiosRequest(mockUrl, 'put', mockParams, mockData, mockConfig);
+        await axiosRequest(mockURL, 'put', mockParams, mockData, mockConfig);
         expect(axiosInstance.request).toHaveBeenCalledWith({
             ...mockConfig,
             data: mockData,
             method: 'put',
             params: mockParams,
-            url: mockUrl,
+            url: mockURL,
         });
     });
 });
