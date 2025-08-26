6472b762d0df9955b769405793eaa9fe67be364c
kiki-kanri <a470666@gmail.com>
2025-08-26 10:49:14 +0800

    chore: update `hash.ts` to align with new package version

--- src/hash.ts
+++ src/hash.ts
@@ -18,9 +18,23 @@ import {
     sha3_384,
     sha3_512,
 } from '@noble/hashes/sha3';
-import { bytesToHex } from '@noble/hashes/utils';
+import {
+    bytesToHex,
+    utf8ToBytes,
+} from '@noble/hashes/utils';
 
-export const sha3224 = (data: string | Uint8Array) => bytesToHex(sha3_224(data));
-export const sha3256 = (data: string | Uint8Array) => bytesToHex(sha3_256(data));
-export const sha3384 = (data: string | Uint8Array) => bytesToHex(sha3_384(data));
-export const sha3512 = (data: string | Uint8Array) => bytesToHex(sha3_512(data));
+export function sha3224(data: string | Uint8Array) {
+    return bytesToHex(sha3_224(typeof data === 'string' ? utf8ToBytes(data) : data));
+}
+
+export function sha3256(data: string | Uint8Array) {
+    return bytesToHex(sha3_256(typeof data === 'string' ? utf8ToBytes(data) : data));
+}
+
+export function sha3384(data: string | Uint8Array) {
+    return bytesToHex(sha3_384(typeof data === 'string' ? utf8ToBytes(data) : data));
+}
+
+export function sha3512(data: string | Uint8Array) {
+    return bytesToHex(sha3_512(typeof data === 'string' ? utf8ToBytes(data) : data));
+}
