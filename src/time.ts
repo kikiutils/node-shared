5aab046c35cfeb9bc758f822e918d6158d84dbdd
kiki-kanri <a470666@gmail.com>
2025-11-18 10:37:31 +0800

    chore: rename `delay` to `abortableDelay`

--- src/time.ts
+++ src/time.ts
@@ -1,4 +1,4 @@
-export function delay(ms: number, signal?: AbortSignal) {
+export function abortableDelay(ms: number, signal?: AbortSignal) {
     return new Promise((resolve) => {
         const timeout = setTimeout(resolve, ms);
         if (signal) {
